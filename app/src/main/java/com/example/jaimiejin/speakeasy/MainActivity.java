d5024d64b8f35dbedb6be18b23f38017d9e1b090
Jaimie Jin <jaimiejin@D-10-19-113-255.dhcp4.washington.edu>
2018-10-21 11:37:47 -0700

    added Text.java and gson

--- app/src/main/java/com/example/jaimiejin/speakeasy/MainActivity.java
+++ app/src/main/java/com/example/jaimiejin/speakeasy/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.jaimiejin.speakeasy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.gson.Gson;
+
+import java.util.Scanner;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gson gson = new Gson();
+        int[] ints = {1, 2, 3, 4, 5, 6};
+        String[] strings = {"cat", "bat", "dog", "hog", "bow","boat"};
+        for (int i = 0; i < strings.length; i++){
+            Text text = gson.fromJson("{\"text\": \""+ strings[i] + "\", \"id\": \"" + ints[i] + "\"}", Text.class);
+            System.out.println(gson.toJson(text));
+        }
+        Scanner console = new Scanner(System.in);
+
+
+
     }
+
 }
 
 
---
+++ app/src/main/java/com/example/jaimiejin/speakeasy/Text.java
@@ -0,0 +1,11 @@
+package com.example.jaimiejin.speakeasy;
+
+public class Text {
+    public int id;
+    public String text;
+
+    public void Identity(String name, int id) {
+        this.text = name;
+        this.id = id;
+    }
+}
